ec70100ee4c9f36af226bdd7728a703843363ea6
sdkn104 <sdkn104@yahoo.co.jp>
2018-11-23 09:13:52 +0900

    add

--- main.py
+++ main.py
@@ -77,4 +77,7 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    import logging
+    logging.basicConfig(filename='/home/sdkn104home/flask.log',level=logging.INFO)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True) #
