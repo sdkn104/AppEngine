cd719a7d89dd5056f5dca3b64b92f917c5a5d518
unknown <sdkn104@yahoo.co.jp>
2018-12-01 12:33:17 +0900

    modify

--- extra/importStockCsv.py
+++ extra/importStockCsv.py
@@ -1,6 +1,7 @@
 import re
 import io
 import datetime
+import pprint
 
 #from memory_profiler import profile
 #@profile
@@ -15,31 +16,46 @@ def datefmt(s):
   a = s.replace("/","-").split("-")
   return "%s-%02d-%02d" % (a[0], int(a[1]), int(a[2]))
 
+# read CSV and retern list of lists
 def getDataCsv(csvfile, start, end):
   import pandas as pd  # require about 60MB memory
   ptn_sbi = r"約定日,銘柄,銘柄コード,市場,"
+  ptn_adsbi = r"入出金日,区分,摘要,出金額,入金額,振替出金額,振替入金額"
   ptn_ch  = r"約定日,受渡日,銘柄コード,銘柄名,市場,"
   ptn_us  = r"約定日,受渡日,ティッカー,銘柄名,"
   ptn_adus = r"受渡日,約定日,口座区分,取引区分,対象証券名,決済通貨,単価,数量［株 /口］,受渡金額（受取）"
   ptn_adjp = r"受渡日,約定日,取引区分,口座区分,対象証券名,単価［円/％］,数量［株/口/額面］,受渡金額（受取）"
+  # read csv file
   with open(csvfile, "r", encoding="shift_jis") as f:
     lines = f.readlines()
-  if re.search(ptn_sbi, "".join(lines)):
+  #print("".join(lines).replace('"',''))
+  # check csv type
+  if re.search(ptn_sbi, "".join(lines).replace('"','')):
+    print("SBI TRADE")
     ptn = ptn_sbi
-  elif re.search(ptn_ch, "".join(lines)):
+  elif re.search(ptn_adsbi, "".join(lines).replace('"','')):
+    print("SBI SHARE")
+    ptn = ptn_adsbi
+  elif re.search(ptn_ch, "".join(lines).replace('"','')):
+    print("RAKUTEN CHINA TRADE")
     ptn = ptn_ch
-  elif re.search(ptn_us, "".join(lines)):
+  elif re.search(ptn_us, "".join(lines).replace('"','')):
+    print("RAKUTEN US TRADE")
     ptn = ptn_us
-  elif re.search(ptn_adus, "".join(lines)):
+  elif re.search(ptn_adus, "".join(lines).replace('"','')):
+    print("RAKUTEN US SHARE")
     ptn = ptn_adus
-  elif re.search(ptn_adjp, "".join(lines)):
+  elif re.search(ptn_adjp, "".join(lines).replace('"','')):
+    print("RAKUTEN JP SHARE")
     ptn = ptn_adjp
   else:
-    ptn = r"xxx"
+    print("error: csv header no match")
+    exit(1)
+  # skip before header line, and read CSV
   with io.StringIO("") as sio:
     flag = False
     for l in lines:
-      if re.match(ptn, l):
+      if re.match(ptn, l.replace('"','')):
         flag = True
       if flag == True:
         sio.write(l)
@@ -60,6 +76,23 @@ def getDataCsv(csvfile, start, end):
       code = row["銘柄コード"]
       comment = code
       mat.append([date, name, account, paid, amount, price, comment])
+  elif ptn == ptn_adsbi:
+    for index, row in df.iterrows():
+      #print(row)
+      date = datefmt(row["入出金日"])
+      t = row["摘要"]
+      if not re.search(r"配当金",t):
+        continue
+      name = re.sub(r"^.*配当金\s+(.*)$","\\1", t)
+      account = "ETRADE"
+      if row["区分"] == "入金":
+        paid = tofloat(row["入金額"]) * -1
+      elif row["区分"] == "出金":
+        paid = tofloat(row["出金額"])
+      amount = 0
+      price = 0
+      comment = row["摘要"]
+      mat.append([date, name, account, paid, amount, price, comment])
   elif ptn == ptn_us:
     for index, row in df.iterrows():
       #print(row)
@@ -69,13 +102,13 @@ def getDataCsv(csvfile, start, end):
       sign = 1 if row["取引区分"][0] == "買" else (-1 if row["取引区分"][0] == "売" else 0)
       rate = tofloat(row["為替レート"])
       amount = tofloat(row["数量［株］"]) * sign
-      price = tofloat(row["単価"]) * rate
-      paid = tofloat(row["受渡金額"]) * sign * float(rate)
+      price = tofloat(row["単価［USドル］"]) * rate
+      paid = tofloat(row["受渡金額［USドル］"]) * sign * float(rate)
       code = row["ティッカー"]
       comment = "%s USDrate=%g" % (code, rate)
       mat.append([date, name, account, paid, amount, price, comment])
       name = "USDJPY"
-      amount = tofloat(row["受渡金額"]) * sign * -1
+      amount = tofloat(row["受渡金額［USドル］"]) * sign * -1
       price = rate
       paid = amount * rate
       comment = code
@@ -121,20 +154,28 @@ def getDataCsv(csvfile, start, end):
       price = 0
       comment = "配当 USDrate=%g" % (rate)
       mat.append([date, name, account, paid, amount, price, comment])
+      comment = name
       name = "USDJPY"
       paid = paid * -1
       amount = paid / rate
       price = rate
-      comment = name
       mat.append([date, name, account, paid, amount, price, comment])
 
+  # filter by start, end dates
+  #pprint.pprint(mat, width=200,depth=2)
   s_start = start.strftime("%Y-%m-%d")
   s_end = end.strftime("%Y-%m-%d")
-  #mat = [m for m in mat if m[0] >= s_start and m[0] <= s_end]
+  mat = [m for m in mat if m[0] >= s_start and m[0] <= s_end]
   return mat
 
 if __name__ == "__main__":
-  values = getDataCsv("adjp.csv",datetime.datetime(2016,2,1), datetime.datetime(2018,12,27))
-  print(values)
-  import myGspread
-  myGspread.insertRows(values, "運用履歴", "data", 0)
+  import sys
+  file = sys.argv[1]
+  start = datetime.datetime(1970,1,1)
+  end = datetime.datetime.today() - datetime.timedelta(days=2)
+  values = getDataCsv(file, start, end)
+  pprint.pprint(values, width=200, depth=2)
+  import myGSpread
+  myGSpread.insertRows(values, "運用履歴", "data", 0)
+  input("enter any key to exit")
+
