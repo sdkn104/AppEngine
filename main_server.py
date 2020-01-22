5a4538445f5bc31fa8f1f4c50786779b15e0a748
sdkn104 <sdkn104@yahoo.co.jp>
2020-01-23 07:07:28 +0900

    add dl_xxx

--- main_server.py
+++ main_server.py
@@ -3,6 +3,7 @@
 #
 
 from flask import Flask, request
+import subprocess
 
 import myGmail
 import myBigQuery
@@ -11,16 +12,18 @@ import checkAlive
 import sendJamInfoToBigQuery
 import private
 
+staticFolder = "/home/sdkn104home/static"
+
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = Flask(__name__)
+app = Flask(__name__, static_folder=staticFolder)
 
 
 @app.route("/")
 @app.route("/"+private.project_app)
 @app.route("/"+private.project_app+"/")
 def hello():
-    return 'Hello World! This page is created by AppEngine.'
+    return 'Hello World! This page is created by Flask on Compute Engine.'
 
 @app.route("/sendJamInfo")
 @app.route("/"+private.project_app+"/sendJamInfo")
@@ -67,12 +70,32 @@ def sendStock():
     myStock.sendToBQ(names_jp, names_bloom, "stock_rcv")
     return 'done'
 
+@app.route("/dl_top")
+@app.route("/"+private.project_app+"/dl_top")
+def download_top():
+    return "<form action='/dl_download'><input type='text'></form>"
+
+@app.route("/dl_ls")
+@app.route("/"+private.project_app+"/dl_ls")
+def download_ls():
+    proc = subprocess.run(["ls", "-l", staticFolder+"/uploads"], stdout=subprocess.PIPE)
+    return proc.stdout
+
+
+@app.route("/dl_download")
+@app.route("/"+private.project_app+"/dl_download")
+def download_download():
+    url = request.args.get('url', '')
+    fn = request.args.get('fn', '')
+    proc = subprocess.Popen(["curl","-m","1800","-o",staticFolder+"/uploads/"+fn,url])
+    return "ss"
 
 @app.route("/"+private.project_app+"/gmail_test")
 def gmail_test():
     myGmail.test()
     return 'sent'
 
+@app.route("/test")
 @app.route("/"+private.project_app+"/test")
 def test():
     return 'test'
@@ -92,4 +115,4 @@ if __name__ == '__main__':
     logging.basicConfig(filename=logdir+'flask.log',level=logging.INFO)
     #logging.basicConfig(filename=logdir+'flask.log',level=logging.DEBUG)
     #app.run(host='127.0.0.1', port=8080, debug=True)
-    app.run(host='0.0.0.0', port=80, debug=False) #
+    app.run(host='0.0.0.0', port=80, debug=True) #
