1e99ab9bdcae1df6192f62196cc6f816badd14a8
sdkn104 <sdkn104@yahoo.co.jp>
2021-02-21 09:51:18 +0900

    add cron script

---
+++ set_cron_server.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# setting cron for server
+
+# !!!!!!! This script is not used NOW !!!! @@@@@@@
+
+# 登録したいジョブ
+cron_job_line="23 3 * * * python3 /home/sdkn104home/AppEngine/yahooMailClean.py"
+
+# crontabファイル
+cron_file="/var/spool/cron/crontabs/root"
+
+## crontabファイル準備
+
+# 無ければ作る
+[ -f ${cron_file} ] && touch ${cron_file}
+
+## タスク登録
+
+# 既に登録されているかどうかを判定
+cron_job_line_for_grep="${cron_job_line}"
+if [ `grep "${cron_job_line_for_grep}" "${cron_file}" | wc -l` -eq 0 ] ; then
+  echo "not registered yet. begin registering..."
+  echo "${cron_job_line}" >> "${cron_file}"
+else
+  echo "already registered."
+fi
+
+# cron再起動
+service cron restart
+
+echo "registering finished."
+
